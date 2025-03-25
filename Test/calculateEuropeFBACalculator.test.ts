da7dffb57467230b0fb7ac5ffa68f5825def6879
lizongxiao <158638447@qq.com>
2025-03-25 19:01:44 +0800

    update

--- Test/calculateEuropeFBACalculator.test.ts
+++ Test/calculateEuropeFBACalculator.test.ts
@@ -1,4 +1,4 @@
-import { EuropeFBACalculator } from "../Calculate/EuropeFBACalculator";
+import { EuropeFBACalculator } from "../Calculate/calculateEuropeFBACalculator";
 
 describe("EuropeFBACalculator", () => {
   test("英国小包裹运费计算", () => {
@@ -16,5 +16,15 @@ describe("EuropeFBACalculator", () => {
   test("包裹小件判断", () => {
     const result = EuropeFBACalculator.calcFbaFee("UK", 300, 240, 100, 1400);
     expect(result?.[2]).toBe("smallParcel");
+  });
+
+  test("it运费计算", () => {
+    const result = EuropeFBACalculator.calcFbaFee("IT", 300, 240, 100, 1400);
+    expect(result?.[0]).toBeCloseTo(5.81);
+  });
+
+  test("es运费计算", () => {
+    const result = EuropeFBACalculator.calcFbaFee("ES", 300, 240, 100, 1400);
+    expect(result?.[0]).toBeCloseTo(4.27);
   });
 });
